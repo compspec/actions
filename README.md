911f38bfd0bf3160ff68ac69baf5fa0ec50358a7
vsoch <vsoch@users.noreply.github.com>
2024-02-21 15:11:24 -0700

    add: validate-schema action
    Problem: we need a central way for schemas to validate in CI
    Solution: Github actions!
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,22 @@
+# Compspec Actions
+
+<p align="center">
+  <img height="300" src="https://raw.githubusercontent.com/compspec/spec/main/img/compspec-circle.png">
+</p>
+
+These are shared GitHub actions for plugins and compspec libraries (and other parties of interest!) to use for validating and generating compatibility specifications. We currently include:
+
+ - [validate-schema](validate-schema): Validate a compatibility plugin schema.
+
+## License
+
+HPCIC DevTools is distributed under the terms of the MIT license.
+All new contributions must be made under this license.
+
+See [LICENSE](https://github.com/converged-computing/cloud-select/blob/main/LICENSE),
+[COPYRIGHT](https://github.com/converged-computing/cloud-select/blob/main/COPYRIGHT), and
+[NOTICE](https://github.com/converged-computing/cloud-select/blob/main/NOTICE) for details.
+
+SPDX-License-Identifier: (MIT)
+
+LLNL-CODE- 842614
---
+++ validate-schema/scripts/validate.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import re
+import json
+import jsonschema
+
+here = os.path.abspath(__file__)
+root = os.path.dirname(os.path.dirname(here))
+
+# Validation for the compspec.json files, a compatibility group schema,
+# is done with an existing standard, json graph format
+schema_file = os.path.join(here, "json-graph-schema_v2.json")
+
+
+def read_json(filename):
+    with open(filename, "r") as fd:
+        content = json.loads(fd.read())
+    return content
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="Compatibility Schema Validator",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument("schema", help="path to schema file to validate")
+    return parser
+
+
+def main(root):
+    """
+    Validate the format of WIP prototype specs.
+    """
+    parser = get_parser()
+
+    # If an error occurs while parsing the arguments, the interpreter will exit with value 2
+    args, extra = parser.parse_known_args()
+
+    # Show args to the user
+    print("     schema: %s" % args.schema)
+
+    to_validate = os.path.abspath(args.schema)
+    if not os.path.exists(to_validate):
+        sys.exit(f"{to_validate} does not exist.")
+
+    schema = read_json(schema_file)
+    spec = read_json(to_validate)
+
+    # Validate
+    print(f"⭐️ Validating spec {to_validate}")
+
+    try:
+        spec = read_json(to_validate)
+    except:
+        sys.exit("Invalid: cannot read json file")
+    jsonschema.validate(spec, schema=schema)
+
+
+if __name__ == "__main__":
+    main()
